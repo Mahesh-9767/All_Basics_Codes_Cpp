1670205299178aac91eb03d6462103ab87d038e0
Mahesh-9767 <137029084+Mahesh-9767@users.noreply.github.com>
2023-09-15 22:06:15 +0530

    Add_Two_Number.cpp

--- Add_Two_Number.cpp
+++ Add_Two_Number.cpp
@@ -1,4 +1,5 @@
-//Add two numbers of the integer type
+//Program:-Add two numbers of the integer type
+//Date:15 sep 2023
 #include<iostream>
 using namespace std;
 int main()
@@ -9,4 +10,42 @@ int main()
 	c=a+b;
 	cout<<"The addition is:"<<c;
 	return 0;
+}
+
+//Program:-Add two numbers of the real Number
+//Date:15 sep 2023
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c;
+	cout<<"Enter the values:";
+	cin>>a>>b;
+	c=a+b;
+	cout<<"The addition is:"<<c;
+	return 0;
+}
+
+//Program:-ussing function
+//Date:15 sep 2023
+//Add two numbers in C++ using a function
+//Let's use this approach to create a program that adds two numbers given by the user. The output of this program is totally the same as the output of the very first program given over here.
+
+#include<iostream>
+using namespace std;
+
+int addFun(int, int);
+int main()
+{
+    int num1, num2, add;
+    cout<<"Enter Two Numbers: ";
+    cin>>num1>>num2;
+    add = addFun(num1, num2);
+    cout<<"\nResult = "<<add;
+    cout<<endl;
+    return 0;
+}
+int addFun(int a, int b)
+{
+    return (a+b);
 }
