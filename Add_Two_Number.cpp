4478cfa82d4f4bfdcb2ac0b3faf9253abcf867b8
Mahesh-9767 <137029084+Mahesh-9767@users.noreply.github.com>
2023-09-15 21:01:44 +0530

    Add files via upload

---
+++ Add_Two_Number.cpp
@@ -0,0 +1,12 @@
+//Add two numbers of the integer type
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"Enter the values:";
+	cin>>a>>b;
+	c=a+b;
+	cout<<"The addition is:"<<c;
+	return 0;
+}
