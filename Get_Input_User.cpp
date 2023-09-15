6050a7d840da17466767bd2f84a8da110c2d9efb
Mahesh-9767 <137029084+Mahesh-9767@users.noreply.github.com>
2023-09-15 16:19:46 +0530

    Get_Input_User.cpp

--- Get_Input_User.cpp
+++ Get_Input_User.cpp
@@ -1,4 +1,4 @@
-//Program:- Take input from user
+//Program:- Take a integer input from user 
 //Date:14 sep 2023
 #include<iostream>
 using namespace std;
@@ -9,4 +9,30 @@ int main()
 	cin>>val;
 	cout<<"Your Entered value is:"<<val;
 	return 0;
+}
+
+//Program:- Take a character input from user 
+//Date:15 sep 2023
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<<"Enter a charcter:"<<endl;
+	cin>>ch;
+	cout<<"Your Enterd Char:"<<ch;
+	return 0;
+}
+
+//Program:- Take a string input from user 
+//Date:15 sep 2023
+#include<iostream>
+using namespace std;
+int main(){
+char str[5];
+cout<<"Enter a string:"<<endl;
+gets(str);    // this use for to craete a spaces
+cin>>str;
+cout<<"Your enterd string is:"<<str;
+return 0;
 }
